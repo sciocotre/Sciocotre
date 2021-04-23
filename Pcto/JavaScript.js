570b879c73cc42a1a4ceaeb09f13cc919ce3825c
sciocotre <shock03mn@gmail.com>
2021-04-23 16:50:06 +0200

    aggiungo esercizio per 24/04

---
+++ Pcto/JavaScript.js
@@ -0,0 +1,9 @@
+function visualizza1(){
+    document.getElementById("p1").hidden = !document.getElementById("p1").hidden;
+}
+function visualizza2(){
+    document.getElementById("p2").hidden = !document.getElementById("p2").hidden;
+}
+function visualizza3(){
+    document.getElementById("p3").hidden = !document.getElementById("p3").hidden;
+}
